98d59eb2f47ceab4ffd9583edd216704c375078a
JamesCode-Ts <jameslob42@gmail.com>
2025-06-14 23:23:58 +0300

    lombok

--- src/main/java/com/java360/pmanager/domain/entity/Project.java
+++ src/main/java/com/java360/pmanager/domain/entity/Project.java
@@ -1,9 +1,15 @@
 package com.java360.pmanager.domain.entity;
 
 import com.java360.pmanager.domain.model.ProjectStatus;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.Objects;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class Project {
 
@@ -20,79 +26,8 @@ public class Project {
 
     private ProjectStatus status;
 
-    public Project(String id, String name, String description, LocalDate initialDate, LocalDate finalDate, ProjectStatus status) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.initialDate = initialDate;
-        this.finalDate = finalDate;
-        this.status = status;
-    }
-
-    public Project(){
 
 
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDate getInitialDate() {
-        return initialDate;
-    }
-
-    public void setInitialDate(LocalDate initialDate) {
-        this.initialDate = initialDate;
-    }
-
-    public LocalDate getFinalDate() {
-        return finalDate;
-    }
-
-    public void setFinalDate(LocalDate finalDate) {
-        this.finalDate = finalDate;
-    }
-
-    public ProjectStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(ProjectStatus status) {
-        this.status = status;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Project project = (Project) o;
-        return Objects.equals(id, project.id) && Objects.equals(name, project.name) && Objects.equals(description, project.description) && Objects.equals(initialDate, project.initialDate) && Objects.equals(finalDate, project.finalDate) && status == project.status;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, description, initialDate, finalDate, status);
-    }
 }
 
 
