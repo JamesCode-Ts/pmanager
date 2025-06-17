7580d4e5e4dfdf4ec91390efcdd2a3b89931ca1f
JamesCode-Ts <jameslob42@gmail.com>
2025-06-17 20:53:05 +0300

    creat new project p/2

--- src/main/java/com/java360/pmanager/domain/applicationservice/ProjectService.java
+++ src/main/java/com/java360/pmanager/domain/applicationservice/ProjectService.java
@@ -3,11 +3,18 @@ package com.java360.pmanager.domain.applicationservice;
 import com.java360.pmanager.domain.entity.Project;
 import com.java360.pmanager.domain.infrastructure.dto.SaveProjectData;
 import com.java360.pmanager.domain.model.ProjectStatus;
+import com.java360.pmanager.domain.repository.ProjectRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ProjectService {
 
+    private final ProjectRepository projectRepository;
+
+    @Transactional
     public Project createProject(SaveProjectData saveProjectData) {
 
         Project project = Project
@@ -19,6 +26,7 @@ public class ProjectService {
                 .status(ProjectStatus.PENDING)
                 .build();
 
+        projectRepository.save(project);
         return  project;
 
     }
