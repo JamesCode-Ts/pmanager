7589c5314eacb8763b480214d784c7a4d7bd71b8
JamesCode-Ts <jameslob42@gmail.com>
2025-06-27 02:09:01 +0300

    Bean validation API

--- src/main/java/com/java360/pmanager/domain/infrastructure/dto/SaveProjectDataDTO.java
+++ src/main/java/com/java360/pmanager/domain/infrastructure/dto/SaveProjectDataDTO.java
@@ -2,16 +2,30 @@ package com.java360.pmanager.domain.infrastructure.dto;
 
 import com.java360.pmanager.domain.model.ProjectStatus;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.NonNull;
 
 import java.time.LocalDate;
 
 @Data
 public class SaveProjectDataDTO {
 
+    @NotNull
+    @Size(min = 1, max = 80)
     private final String name;
+
+    @NotNull
+    @Size(min = 1, max = 150)
     private final String description;
+
+    @NotNull
     private final LocalDate initialDate;
+
+    @NotNull
     private final LocalDate finalDate;
+
     private final String status;
 }
+
