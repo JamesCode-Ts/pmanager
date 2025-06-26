58d3eaccfc7fd8d721bcc2e6139e5ee6c6284402
JamesCode-Ts <jameslob42@gmail.com>
2025-06-26 23:05:25 +0300

    type of RequestExeception

--- src/main/java/com/java360/pmanager/domain/infrastructure/dto/exception/AppExeceptionHandler.java
+++ src/main/java/com/java360/pmanager/domain/infrastructure/dto/exception/AppExeceptionHandler.java
@@ -13,23 +13,47 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class AppExeceptionHandler extends ResponseEntityExceptionHandler {
 
 
+    @ExceptionHandler(value = RequestException.class)
+    public ResponseEntity<Object> handleRequestException(RequestException ex, WebRequest request) {
+        ServletWebRequest servletWebRequest = (ServletWebRequest) request;
+
+        return handleException(ex, ex.getErrorCode(), ex.getMessage(),HttpStatus.BAD_REQUEST,request);
+
+    }
+
+
+
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<Object> handleGenericException(Exception ex, WebRequest request) {
+        ServletWebRequest servletWebRequest = (ServletWebRequest) request;
+
+        return handleException(ex, null, ex.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR, request);
+
+    }
+
+
+    private ResponseEntity<Object> handleException(
+            Exception ex,
+            String errorCode,
+            String message,
+            HttpStatus status,
+            WebRequest request
+    ){
         ServletWebRequest servletWebRequest = (ServletWebRequest) request;
 
         return handleExceptionInternal(
                 ex,
                 RestError
                         .builder()
-                        .errorCode("xyz")
-                        .errorMessage(ex.getMessage())
-                        .status(HttpStatus.BAD_REQUEST.value())
+                        .errorCode(errorCode)
+                        .errorMessage(message)
+                        .status(status.value())
                         .path(servletWebRequest.getRequest().getRequestURI())
                         .build(),
                 new HttpHeaders(),
-                HttpStatus.BAD_REQUEST,
+                status,
                 request
-         );
+        );
 
     }
 }
