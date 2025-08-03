fb3c99571176e9a4586a4a317ae9d2e338476cac
JamesCode-Ts <jameslob42@gmail.com>
2025-08-03 16:02:49 -0300

    Task DTOs created

---
+++ src/main/java/com/java360/pmanager/domain/infrastructure/dto/TaskDTO.java
@@ -0,0 +1,27 @@
+package com.java360.pmanager.domain.infrastructure.dto;
+
+import com.java360.pmanager.domain.entity.Task;
+import com.java360.pmanager.domain.model.TaskStatus;
+import lombok.Data;
+
+@Data
+public class TaskDTO {
+
+    private final String id;
+    private final String title;
+    private final String description;
+    private final Integer numberOfDays;
+    private final TaskStatus status;
+
+    public static TaskDTO create(Task task){
+        return new TaskDTO(
+                task.getId(),
+                task.getTitle(),
+                task.getDescription(),
+                task.getMemberOfDays(),
+                task.getStatus()
+
+        );
+    }
+
+}
